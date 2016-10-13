4e80be19654a385b0deb47e7e72781a68cf5f158
Magda Ochej <zaba1980@yahoo.com>
2016-10-13 19:34:17 +0200

    pierwszy commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	console.log('DOM załadowany - można się bawić');
+});
+var span = $('span');
+span.each(function(index, element) {
+});
+span.each(function(index, element) {
+	if(index % 2 == 0) {
+		$(element).css('color', 'red');
+	};
+});
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+$(element).append(button) 
+});
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
